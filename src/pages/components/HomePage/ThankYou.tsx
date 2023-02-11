37a1f662c121e77623ebb768e410e71ceba1a7e7
Dri Espindola <103765212+driespindola@users.noreply.github.com>
2023-02-11 14:40:21 -0300

    update

--- src/pages/components/HomePage/ThankYou.tsx
+++ src/pages/components/HomePage/ThankYou.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from "react";
 import { Option } from "./Card";
+import Image from "next/image";
 
 interface Props {
     selectedOption: Option | null
@@ -9,9 +10,11 @@ const ThankYou: FC<Props> = ({ selectedOption }) => {
     return(
         <div className="mt-[195px] mb-[195px] max-w-[326px] max-h-[361px] md:max-w-[411px] md:max-h-[413px] bg-gradient-to-br from-[#212832] to-[#19202A] rounded-3xl animate-myAnim">
             <div className="mt-[34px] md:mt-[45px] mx-[91px] md:ml-[120px] md:mr-[129px]">
-                <img
-                src="illustration-thank-you.svg"
-                alt="star"   
+                <Image
+                    width={162}
+                    height={108}
+                    src="illustration-thank-you.svg"
+                    alt="star"   
                 />
             </div>
             <div className="flex justify-center items-center bg-[#272E38] mt-[23px] mx-[80px] md:mt-[30px] md:mx-[109px] px-[5px] w-[165px] h-[34px] md:w-[193px] md:h-[34px] font-overpass text-[15px] tacking-[-0.035em] md:tracking-[-0.005em] text-[#FC7613] rounded-full">
